4c7fe8da582363e24f12d68573205215c1865f30
Simon Haycock <shaycock@hotels.com>
2019-05-12 10:39:35 +0100

    remove solution

--- AOC1.playground/Contents.swift
+++ AOC1.playground/Contents.swift
@@ -2,13 +2,4 @@ import UIKit
 
 let puzzleInput = Puzzle.input
 let puzzleInputLines = puzzleInput.components(separatedBy: "\n")
-var total: Int = 0
-for line in puzzleInputLines {
-    guard let intFromString = Int(line.dropFirst()) else { fatalError("no int from line: \(line)") }
-    if line.hasPrefix("+") {
-        total += intFromString
-    } else {
-        total -= intFromString
-    }
-}
-print(total)
+
